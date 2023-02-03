e72b9b40ea693a8c4bde4cc158b55e5ff35dbcb2
SatyamRandawa <satyamrandwa141@gmail.com>
2023-02-03 17:07:45 +0530

    Agent push

--- CreateDIDs/index.js
+++ CreateDIDs/index.js
@@ -7,6 +7,7 @@ const router = require('./routes/route')
  const cors = require('cors')
 
 
+
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,6 +27,6 @@ mongoose.connect("mongodb+srv://satyamRandawa:Loveyam@cluster0.tfry3tr.mongodb.n
  app.use('/',isAuth, router);
 //sad
 
-app.listen(process.env.Port || 2001, function () {
-    console.log('App running on port ' + (process.env.PORT || 2001))
+app.listen(process.env.Port || 2007, function () {
+    console.log('App running on port ' + (process.env.PORT || 2007))
 });
