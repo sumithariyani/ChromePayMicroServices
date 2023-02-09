63de11ed5581420aed943cfdfff1cf9c8f533cef
SatyamRandawa <satyamrandwa141@gmail.com>
2023-02-09 15:15:41 +0530

    sumit update

---
+++ BlockedDIDs/routes/route.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const {
+    createCustomerByOrg1, createCustomerByOrg2, new_verify_customer, Cust_Linked_Srevice_send_OTP, Cust_Linked_Srevice, Resend_otp, orgList, globalImageUploader
+} = require('../controller/DIDController');
+
+
+router.post("/registerDID1", createCustomerByOrg1)
+router.post("/registerDID2",createCustomerByOrg2 )
+router.post("/verifyOTP", new_verify_customer)
+router.post("/Linked-send-otp", Cust_Linked_Srevice_send_OTP)
+router.post("/Linked-service", Cust_Linked_Srevice)
+router.post("/Resend_otp/:phone", Resend_otp)
+router.get("/orgList", orgList)
+router.post("/ImageUploader",globalImageUploader)
+
+
+
+module.exports = router
